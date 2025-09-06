c7b3213f990cf1d03462754e5cb6df6a38fa6f1e
Mee1296 <weeraphat61100@gmail.com>
2025-09-06 22:05:38 +0700

    feat: add auth and cookie

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+
 // Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -12,8 +14,16 @@ const app = express();
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
+//Route files
 const massageshops = require('./routes/massageshops')
+const auth = require('./routes/auth');
+
+//Route
 app.use('/api/v1/massageshops', massageshops);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server is running in ', process.env.NODE_ENV, ' mode on port ', PORT));
