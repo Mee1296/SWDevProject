54762c2588ad36c6e57f85ee54c0eb0c094ed7b3
Mee1296 <weeraphat61100@gmail.com>
2025-09-08 16:24:01 +0700

    feat: add authentication

--- routes/massageshops.js
+++ routes/massageshops.js
@@ -2,8 +2,9 @@ const express = require('express');
 const {getMassageshops, getMassageshop, createMassageshop, updateMassageshop, deleteMassageshop} = require('../controllers/massageshops')
 const router = express.Router();
 
+const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(getMassageshops).post(createMassageshop);
-router.route('/:id').get(getMassageshop).put(updateMassageshop).delete(deleteMassageshop);
+router.route('/').get(getMassageshops).post(protect, authorize('admin'), createMassageshop);
+router.route('/:id').get(getMassageshop).put(protect, authorize('admin'), updateMassageshop).delete(protect, authorize('admin'), deleteMassageshop);
 
 module.exports=router;
