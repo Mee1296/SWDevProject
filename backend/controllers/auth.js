d6cc66e92e258f1c0307759cedae389d2779d9b3
Mee1296 <weeraphat61100@gmail.com>
2025-10-26 23:03:49 +0700

    add securrity

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -31,27 +31,27 @@ exports.register = async (req, res, next) => {
 //@route  POST /api/v1/auth/login
 //@access Public
 exports.login = async (req, res, next) => {
-    const { email, password } = req.body;
-    //Validate email & password
-    if (!email || !password) {
-        return res.status(400).json({ success: false, error: 'Please provide an email and password' });
-    }
-        //Check for user
-    const user = await User.findOne({email}).select('+password');
-    if (!user) {
-        return res.status(400).json({ success: false, error: 'Invalid credentials' });
-    }
-    //Check if password matches
-    const isMatch = await user.matchPassword(password);
+    try {
+        const { email, password } = req.body;
+        //Validate email & password
+        if (!email || !password) {
+            return res.status(400).json({ success: false, error: 'Please provide an email and password' });
+        }
+            //Check for user
+        const user = await User.findOne({email}).select('+password');
+        if (!user) {
+            return res.status(400).json({ success: false, error: 'Invalid credentials' });
+        }
+        //Check if password matches
+        const isMatch = await user.matchPassword(password);
 
-    if(!isMatch) {
-        return res.status(401).json({ success: false, error: 'Invalid credentials' });
+        if(!isMatch) {
+            return res.status(401).json({ success: false, error: 'Invalid credentials' });
+        }
+        sendTokenResponse(user, 200, res);
+    }catch (err) {
+        return res.status(401).json({success: false, msg: 'Cannot convert email or password to string'});
     }
-
-    // const token = user.getSignedJwtToken();
-     
-    // res.status(200).json({ success: true, token});
-    sendTokenResponse(user, 200, res);
 }
 
 const sendTokenResponse = (user, statusCode, res) => {
@@ -69,6 +69,21 @@ const sendTokenResponse = (user, statusCode, res) => {
         success: true,
         token
     })
+}
+
+//@desc     logout user
+//@route    POST /api/v1/auth/logout
+//@access   Private
+exports.logout = async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 }
 
 //@desc     Get current Logged in user
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const reateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 // Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -18,11 +23,31 @@ app.use(express.json());
 //Cookie parser
 app.use(cookieParser());
 
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = reateLimit({
+    windowMs:10 * 60 * 1000, //10 mins
+    max:100
+});
+app.use(limiter);
+
+//Prevent http param pollution
+app.use(hpp());
+
 //cors
 app.use(cors({
   origin: ['http://localhost:3000','http://127.0.0.1:3000'],
   credentials: true
 }));
+
 //Route files
 const massageshops = require('./routes/massageshops')
 const auth = require('./routes/auth');
